7dc0ca6a96dddba608e999b185a6e0b4fc17fdd6
Rodriguez <adm.96.rrm@gmail.com>
2025-05-11 00:12:09 +0200

    Railway

--- resources/views/vehiculos/index.blade.php
+++ resources/views/vehiculos/index.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     
-    <title>Vehículos - {{ config('MiraCar', 'app.name') }}</title>
+    <title>{{ config('app.name', 'MiraCar') }} - Vehículo</title>
     
     <link rel="icon" href="{{ asset('galeria/logo.ico') }}" type="image/x-icon">
     <link rel="shortcut icon" href="{{ asset('galeria/logo.ico') }}" type="image/x-icon">
